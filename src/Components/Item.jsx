1de3c9b01bf8c12354a095c6d9642ba0a8332d7d
Edwin <edwingrow16@gmail.com>
2022-01-25 10:21:26 -0300

    moví los datos a ItemListContainer

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,35 +1,22 @@
-import React, { useEffect, useSelector } from 'react';
+import React from 'react';
 import { Card, Button } from "react-bootstrap";
-
 const Item = ({ productos }) => {
-
-    const [producto, setProducto] = React.useState([])
-    useEffect(() => {
-        setProducto(productos)
-    })
+    const { title, price, image, stock } = productos;
     return (
         <>
-            <div className="container">
-                (
-                {
-                    producto.map(element => {
-                        return (
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src={element.image} />
-                                <Card.Body>
-                                    <Card.Title>{element.title}</Card.Title>
-                                    <Card.Text>
-                                        {element.price}
-                                    </Card.Text>
-                                    <Card.Text>
-                                        stock:{element.stock}
-                                    </Card.Text>
-                                    <Button variant="primary">Ver detalle del producto</Button>
-                                </Card.Body>
-                            </Card>
-                        )})})
-            </div> 
-            </> 
-        )
-};
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={image} />
+                <Card.Body>
+                    <Card.Title>{title}</Card.Title>
+                    <Card.Text>
+                        Precio:{price}
+                    </Card.Text>
+                    <Card.Text>
+                        stock:{stock}
+                    </Card.Text>
+                    <Button variant="primary">Ver detalle del producto</Button>
+                </Card.Body>
+            </Card>
+        </>)
+}
 export default Item;
