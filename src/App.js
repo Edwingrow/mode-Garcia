c1c93c98518ef7a7fbdb6c3828b131ac7bec7511
Edwin <edwingrow16@gmail.com>
2022-02-08 17:16:41 -0300

    Carpeta Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from 'react'
-import Layout from './components/Layout'
-import Home from './components/Home'
-import NoPage from './components/NoPage'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ItemDetailContainer from './components/containers/ItemDetailContainer'
-import Category from './components/Category'
+import AppRouter from './routes/AppRouter'
 import { CartProvider } from './context/CartContext'
 
 function App () {
   return (
     <CartProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />} >
-        <Route index element={<Home />} />
-        <Route path="/productos/:id" element={<ItemDetailContainer />} />
-        <Route path="/category/:id" element={<Category />} /> 
-        <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+      <AppRouter />
     </CartProvider>
   )
 }
